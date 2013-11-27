5ede1505bef585af3eca7405853317f06016c112
root <root@243fd332-3cb7-4356-b4cc-f2eb9687c9df>
2013-11-27 17:58:53 +0000

    This was called Cust_usr.srt
    git-svn-id: svn+ssh://192.168.1.9/var/svn/Frita/freeitathenscode@7 243fd332-3cb7-4356-b4cc-f2eb9687c9df

---
+++ QC_process_dev/Prepare_for_imaging.sh
@@ -0,0 +1,32 @@
+# " get FreeIT.png, move to common image dir ",
+#sudo chown -c root:root FreeIT.png 
+locate FreeIT.png |xargs ls -l
+
+egrep -v '^\s*(#|$)' /etc/fstab |grep swap |grep UUID && echo -e "\n\e[1;31;47mfstab cannot go on image with local UUID reference\e[0m\n"
+
+echo '<ENTER>'
+read xU
+
+# "Remove reference to medibuntu":
+egrep -v '^\s*(#|$)' /etc/apt/sources.list |grep medi && sudo vi /etc/apt/sources.list
+
+# "Use update manager"
+
+sudo apt-get install dialog xscreensaver-gl
+# "(64 Mate only): add OnlyIn Mate;"
+
+# "(32 Xfce only)":
+#sudo apt-get install gnome-system-tools 
+# "(32: save session)"
+
+#sudo add-apt-repository ppa:mozillateam/firefox-next
+#sudo add-apt-repository ppa:otto-kesselgulasch/gimp
+#sudo apt-get install gimp firefox
+
+sudo apt-get install lm-sensors hddtemp ethtool 
+
+# "Before new image out":
+sudo find /root/.pulse /root/.dbus/session-bus -ls -delete
+sudo find /root/ -name ".pulse*" -ls -delete
+find ~/.ssh -not -type d -ls -delete
+
