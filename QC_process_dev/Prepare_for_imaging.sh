aaf16387c78e848f5513abdf5b2a97c8442d04f9
root <root@243fd332-3cb7-4356-b4cc-f2eb9687c9df>
2013-12-19 21:38:22 +0000

    Prepare for imaging tightened up. QC_Backend for 32 bit (xfce) has reference to session file to check
    git-svn-id: svn+ssh://192.168.1.9/var/svn/Frita/freeitathenscode@11 243fd332-3cb7-4356-b4cc-f2eb9687c9df

--- QC_process_dev/Prepare_for_imaging.sh
+++ QC_process_dev/Prepare_for_imaging.sh
@@ -1,33 +1,45 @@
 # " get FreeIT.png, move to common image dir ",
 #sudo chown -c root:root FreeIT.png 
-locate FreeIT.png |xargs ls -l
+FIT='FreeIT.png'
+locate $FIT |xargs ls -l
+echo 'does' $FIT 'file listed appear above?'
+read xU
 
+echo 'look for (absence of) local UUID reference for swap in fstab'
 egrep -v '^\s*(#|$)' /etc/fstab |grep swap |grep UUID && echo -e "\n\e[1;31;47mfstab cannot go on image with local UUID reference\e[0m\n"
-
 echo '<ENTER>'
 read xU
 
 # "Remove reference to medibuntu":
 egrep -v '^\s*(#|$)' /etc/apt/sources.list |grep medi && sudo vi /etc/apt/sources.list
 
-# "Use update manager"
+echo "Use update manager <PAUSING>"
+read xU
 
 sudo apt-get install dialog xscreensaver-gl
 # "(64 Mate only): add OnlyIn Mate;"
 
 # "(32 Xfce only)":
-#sudo apt-get install gnome-system-tools 
-# "(32: save session)"
-
-#sudo add-apt-repository ppa:mozillateam/firefox-next
-#sudo add-apt-repository ppa:otto-kesselgulasch/gimp
-#sudo apt-get install gimp firefox
+sudo apt-get install gnome-system-tools 
 
 sudo apt-get install lm-sensors hddtemp ethtool 
+# "(32: save session)"
+echo 'Notification to save session'
+#ensure existence of : /home/*/.config/xfce4/xfconf/xfce-perchannel-xml/xfce4-session.xml:    <property name="SessionName" type="string" value="Default"/>
+
+sudo add-apt-repository ppa:mozillateam/firefox-next
+sudo add-apt-repository ppa:otto-kesselgulasch/gimp
+sudo apt-get install gimp firefox
 
 # "Before new image out":
 sudo find /root/.pulse /root/.dbus/session-bus -ls -delete
 sudo find /root/ -name ".pulse*" -ls -delete
 find ~/.ssh -not -type d -ls -delete
-# Clear oem .mozilla cache/history
+echo 'Clearing cups settings (if any)'
+for CUPSDEF in /etc/cups/{classes,printers,subscriptions}.conf; do if [ -f ${CUPSDEF}.O ];then sudo cp -v ${CUPSDEF}.O $CUPSDEF;bn=$(basename $CUPSDEF);sudo find /etc/cups/ -name "${bn}*" -exec sudo md5sum {} \; -exec sudo ls -l {} \; ;else :;fi;done
+# 
+
+echo "Remove-ing QC test result files"
+cd
+rm -v QC.log QC.sorted.log 2>/dev/null
 
