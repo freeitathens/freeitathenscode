57999c71a439f6324edb4ff94e81484e28719623
root <root@243fd332-3cb7-4356-b4cc-f2eb9687c9df>
2014-02-25 16:29:47 +0000

    Using a Pz
    git-svn-id: svn+ssh://192.168.1.9/var/svn/Frita/freeitathenscode@20 243fd332-3cb7-4356-b4cc-f2eb9687c9df

--- QC_Process/Prepare_for_imaging.sh
+++ QC_Process/Prepare_for_imaging.sh
@@ -1,49 +1,65 @@
 # " get FreeIT.png, move to common image dir ",
 #sudo chown -c root:root FreeIT.png 
+# *--* Identify box as 32 or 64 bit capable.
+CPU_ADDRESS=32
+CPUFLAGS=$(cat /proc/cpuinfo |grep '^flags')
+for GL in $CPUFLAGS ;do if [ $GL == 'lm' ];then CPU_ADDRESS=64;fi;done
+# *0000000000000000000000000000000000000000000000000000000000000*
+Pauze() {
+    msg=${@:-'<ENTER>'}
+    echo $msg
+    read xU
+}
+
 FIT='FreeIT.png'
 locate $FIT |xargs ls -l
-echo 'does' $FIT 'file listed appear above?'
-read xU
+Pauze 'Look for' $FIT 'listed above.'
 
 echo 'look for (absence of) local UUID reference for swap in fstab'
 egrep -v '^\s*(#|$)' /etc/fstab |grep swap |grep UUID && echo -e "\n\e[1;31;47mfstab cannot go on image with local UUID reference\e[0m\n"
-echo '<ENTER>'
-read xU
+Pauze
 
 # "Remove reference to medibuntu":
 egrep -v '^\s*(#|$)' /etc/apt/sources.list |grep medi && sudo vi /etc/apt/sources.list
 
-echo "Use update manager <PAUSING>"
-read xU
+if [ 0 -eq $(find /etc/apt/sources.list.d/ -type f -name 'mozillateam*' |wc -l) ];then
+    sudo add-apt-repository ppa:mozillateam/firefox-next
+fi
+if [ 0 -eq $(find /etc/apt/sources.list.d/ -type f -name 'otto-kesselgulasch*' |wc -l) ];then
+    sudo add-apt-repository ppa:otto-kesselgulasch/gimp
+fi
+for Pkg in lm-sensors hddtemp ethtool gimp firefox dialog xscreensaver-gl
+do
+    sudo apt-get install $Pkg
+done
 
-sudo apt-get install dialog xscreensaver-gl
-# "(64 Mate only): add OnlyIn Mate;"
+if [ $CPU_ADDRESS -eq 32 ]
+then
+    sudo apt-get install gnome-system-tools 
+    #ensure existence of : /home/*/.config/xfce4/xfconf/
+        #xfce-perchannel-xml/xfce4-session.xml: 
+        #<property name="SessionName" type="string" value="Default"/>
+else
+    grep -o -P '^OnlyShowIn=.*MATE' /usr/share/applications/screensavers/*.desktop 
+fi
+Pauze 'CPU size specifice above <ENTER>'
 
-# "(32 Xfce only)":
-sudo apt-get install gnome-system-tools 
-
-sudo apt-get install lm-sensors hddtemp ethtool 
-# "(32: save session)"
-echo 'Notification to save session'
-#ensure existence of : /home/*/.config/xfce4/xfconf/xfce-perchannel-xml/xfce4-session.xml:    <property name="SessionName" type="string" value="Default"/>
-
-sudo add-apt-repository ppa:mozillateam/firefox-next
-sudo add-apt-repository ppa:otto-kesselgulasch/gimp
-sudo apt-get install gimp firefox
-
-# "Before new image out":
+#Clean up root files that oem used.
 sudo find /root/.pulse /root/.dbus/session-bus -ls -delete
 sudo find /root/.pulse /root/.dbus/session-bus -ls
 sudo find /root/ -name ".pulse*" -ls -delete
 sudo find /root/ -name ".pulse*" -ls
 find ~/.ssh -not -type d -ls -delete
 find ~/.ssh -not -type d -ls
-echo 'Clearing cups settings (if any)'
-for CUPSDEF in /etc/cups/{classes,printers,subscriptions}.conf; do if [ -f ${CUPSDEF}.O ];then sudo cp -v ${CUPSDEF}.O $CUPSDEF;bn=$(basename $CUPSDEF);sudo find /etc/cups/ -name "${bn}*" -exec sudo md5sum {} \; -exec sudo ls -l {} \; ;else :;fi;done
 
+Pauze 'Clearing cups settings (if any)'
+for CUPSDEF in /etc/cups/{classes,printers,subscriptions}.conf; do if [ -f ${CUPSDEF}.O ];then sudo cp -v ${CUPSDEF}.O $CUPSDEF;bn=$(basename $CUPSDEF);sudo find /etc/cups/ -name "${bn}*" -exec sudo md5sum {} \; -exec sudo ls -l {} \; ;else :;fi;done
+#
 echo "Remove-ing QC test result files"
-rm -vi ${HOME}{,/Desktop}/QC*log 2>/dev/null
+rm -vi ${HOME}{,/Desktop}/QC*log
+echo "Verify-ing QC test result files ARE removed"
 find $HOME -name 'QC*log' -ls
+Pauze '** -- ****** Nothing above *******I** -- **'
 
 # Additional options
 #swapoff --all --verbose
@@ -56,5 +72,6 @@ swapon --summary --verbose
 free
 locate iguazu
 lsb_release -a
-#locate xorg.conf
+locate xorg.conf
+set +x
 
