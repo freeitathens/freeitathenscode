77550d4d59502fdfb95c7e556592e5480f5c2624
frodo <frodo@243fd332-3cb7-4356-b4cc-f2eb9687c9df>
2014-07-24 14:36:31 +0000

    Mine is from Mast64
    git-svn-id: svn+ssh://192.168.1.9/var/svn/Frita/freeitathenscode@148 243fd332-3cb7-4356-b4cc-f2eb9687c9df

--- image_scripts/Prepare_for_imaging.sh
+++ image_scripts/Prepare_for_imaging.sh
@@ -1,24 +1,29 @@
 #!/bin/bash
 if [ 0 -lt $(id |grep -o -P '^uid=\d+' |cut -f2 -d=) ]
 then
-    Pauze 'ERROR,Please rerun with sudo or as root'
+    echo 'ERROR, Please rerun with sudo or as root'
     exit 4
 fi
 
+# Establish root of version-controlled code tree.
 temp_sourcebase="${HOME}/freeitathenscode"
 if [ ! -d $temp_sourcebase ]
 then
     read -p 'Location of Freeit code? ' -a temp_sourcebase
     if [ "${temp_sourcebase}." == 'N.' ]
     then
-        exit 12
+        exit 2
     fi
     [[ -d $temp_sourcebase ]] || exit 13
 fi
+
+# Make the version-controlled tree root - sourcebase - an unchangable value,
+#   and let children inherit.
 declare -rx sourcebase=$temp_sourcebase
 
+# Establish directory of Common Functions within sourcebase (vc root)
 temp_codebase=${sourcebase}'/image_scripts'
-echo 'Checking for existence of directory called '$temp_codebase
+#echo 'Checking for existence of directory called '$temp_codebase
 [[ -d $temp_codebase ]] || exit 14
 
 declare -rx codebase=$temp_codebase
@@ -28,10 +33,10 @@ declare -r HOLDIFS=$IFS
 declare -rx Messages_O=$(mktemp -t "Prep_log.XXXXX")
 declare -rx Errors_O=$(mktemp -t "Prep_err.XXXXX")
 declare -x aptcache_needs_update='Y'
-declare -x refresh_git='Y'
 fallback_distro=''
 FreeIT_image='FreeIT.png'
-refresh_update='N'
+declare -x refresh_git='Y'
+refresh_updatedb='N'
 refresh_svn='N'
 
 while getopts 'jd:i:RuVGh' OPT
@@ -39,7 +44,7 @@ do
     case $OPT in
         j)
             Runner_shell_hold=${Runner_shell_hold}'i'
-	    ;;
+        ;;
         d)
             fallback_distro=$OPTARG
             ;;
@@ -50,7 +55,7 @@ do
             export aptcache_needs_update='N'
             ;;
         u)
-            refresh_update='Y'
+            refresh_updatedb='Y'
             ;;
         V)
             refresh_svn='Y'
@@ -105,33 +110,31 @@ Correct_subversion_ssh() {
 }
 
 Set_background() {
+    local Image_file=$1
+    [[ -z "$Image_file" ]] && return 6
+    local Image_dir=$2
+    [[ -z "$Image_dir" ]] && return 9
+    [[ -d "$Image_dir" ]] || return 5
+    shift 2
 
-    [[ -z "$Backgrounds_location" ]] && return 9
-    [[ -d "$Backgrounds_location" ]] || return 5
-    [[ -z "$FreeIT_image" ]] && return 6
-
-    Pauze "Checking background file location: $Backgrounds_location / $FreeIT_image"
-    Have_BG=$(ls -l ${Backgrounds_location}/$FreeIT_image\
-                || find ${Backgrounds_location}/ -name "$FreeIT_image"\
-                || echo 'NADA')
-    if [ "$Have_BG" == 'NADA' ]
+    Pauze "Checking background file location: $Image_dir / $Image_file"
+    Have_BG=$(ls -l ${Image_dir}/$Image_file)
+    if [ $? -gt 0 ]
     then
-        unset xR
-        echo 'Shall I try to retrieve' $FreeIT_image '(Y|N)?' 
-        read xR
-        case $xR in
-            y|Y)
-                sudo cp -iv /home/oem/freeitathenscode/image_scripts/$FreeIT_image\
-                ${Backgrounds_location}/ || return 15
-                ;;
-            *)
-                Pauze 'WARNING,OK, Handle it later...'
-                ;;
-        esac
+        Pauze 'WARNING,OK, Background needs setup. First, searching all subdirs...'
+        find ${Image_dir}/ -name "$Image_file" &
+        Answer='Y'
+        Pause_n_Answer 'Y|N' 'INFO,Shall I try to retrieve '$Image_file' (Default '$Answer')?'
+        if [ "${Answer}." == 'Y.' ]
+        then
+            sudo cp -iv ${codebase}/$Image_file ${Image_dir}/ || return 15
+        else
+            Pauze 'WARNING,OK, Handle it later...'
+        fi
     fi
 }
 
-if [ "${refresh_update}." == 'Y.' ]
+if [ "${refresh_updatedb}." == 'Y.' ]
 then
     updatedb &
 fi
@@ -144,6 +147,7 @@ Get_DISTRO $DISTRO;CDC_RC=$?
 Confirm_DISTRO_CPU $CDC_RC;CDC_RC=$?
 if [ $CDC_RC -gt 0 ]
 then
+    # prettyprint is sourced from Common_functions
     prettyprint '5,31,47,M,n,0' 'Exiting'
     Pauze "See you back soon!"
     exit $CDC_RC
@@ -151,12 +155,11 @@ fi
 
 Answer='Y'
 Pause_n_Answer 'Y|N' 'INFO,Check (absence of) local UUID reference for swap in fstab.(Default '$Answer')?'
-#Pauze 'Check (absence of) local UUID reference for swap in fstab.'
 if [ "${Answer}." == 'Y.' ]
 then
-    egrep -v '^\s*(#|$)' /etc/fstab |grep swap |grep UUID &&\
-	      prettyprint 'n,1,31,47,M,0,n'\
-	      'fstab cannot go on image with local UUID referencer'
+    egrep -v '^\s*(#|$)' /etc/fstab\
+        |grep swap |grep UUID && prettyprint 'n,1,31,47,M,0,n'\
+        'fstab cannot go on image with local UUID reference'
 fi
 
 Pauze 'Checking swap'
@@ -221,13 +224,14 @@ case $DISTRO in
         ;;
 esac
 
-Set_background $FreeIT_image;bg_RC=$?
+Set_background $FreeIT_image $Backgrounds_location
+bg_RC=$?
 case $bg_RC in
     0) backmess='Background setting ok'
     ;;
     5) backmess="Invalid backgrounds directory ${Backgrounds_location}. Set background manually"
     ;;
-    6) backmess='Invalid background filename'
+    6) backmess='Invalid background filename '$FreeIT_image
     ;;
     *) backmess="Serious problems with setting background. RC=${bg_RC}"
     ;;
@@ -343,33 +347,4 @@ then
 fi
 
 set +x
-
-# *--* Confirm Distro name with user *--*
-#Confirm_DISTPU() {
-#    return_value=0
-#
-#    Pauze "WARN,You have a ${DDRESS}-bit box running $DISTRO ."
-
-#    case $DISTRO in
-#        lubuntu)
-#            prettyprint '7,32,47,M,0' 'Valid'
-#            ;;
-#        LinuxMint|mint)
-#            prettyprint '7,32,47,M,0' 'Valid'
-#            DISTRO='mint'
-#            ;;
-#        Ubuntu)
-#            prettyprint '7,32,47,M,0' 'Valid'
-#            ;;
-#        *)
-#            prettyprint '1,31,47,M,0' 'Invalid:'
-#            prettyprint 't,7,31,47,M,0,n' "Problem with Distro Name ${DISTRO}."
-#            Pauze "PROBLEM,(Note, you can run this as $0 distroname)"
-#            return 16
-#            ;;
-#    esac
-#
-#    Pauze "Confirmed Distro ${DISTRO}."
-#    return $return_value
-#}
 
