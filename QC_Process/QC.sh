b9e96e69a0b621cf39356cb78353bbc35a4f0a53
frodo <frodo@243fd332-3cb7-4356-b4cc-f2eb9687c9df>
2014-08-01 23:04:57 +0000

    QC.sh with optional update of QC_Backend pxe/default two new betas normal fs
    git-svn-id: svn+ssh://192.168.1.9/var/svn/Frita/freeitathenscode@157 243fd332-3cb7-4356-b4cc-f2eb9687c9df

--- QC_Process/QC.sh
+++ QC_Process/QC.sh
@@ -1,21 +1,23 @@
 #!/bin/bash
+Down_late=${1:-'N'}
 
 # this script should not perform or report on any QC checks itself
 # it should only update the QC scripts and run the backend
-#   20131126 jxi : update disabled since not sure clients should be required to
-#   have an svn client installed.
 
-# redirecting output keeps the screen clear from confusing messages
-#echo "Downloading latest QC script"
-#svn update ~/freeitathenscode/QC_Process > /dev/null
-
-# $? is the exit status of the previous command
-# svn returns 0 on success and 1 on failure
-#if [ $? == 0 ]; then
-#	echo "Download succeeded"
-#else
-#	echo "Download failed, using existing version"
-#fi
+if [ $Down_late == 'Y' ]
+then
+    echo "Downloading latest QC script"
+    svn update ~/freeitathenscode/QC_Process > /dev/null
+    # redirect (>) output to bitbucket (/dev/null) keeps user 
+    # from being overwhelmed with not-so-relevent messages.
+    if [ $? == 0 ]; then
+        # $? is the exit status of the previous command
+        # svn returns 0 on success and 1 on failure
+        echo "Download succeeded"
+    else
+        echo "Download failed, using existing version"
+    fi
+fi
 
 #if [ ! -e ~/Desktop/Disable_3D.desktop ]; then
 #   ln -s ~/freeitathenscode/QC_Process/Disable_3D.desktop ~/Desktop/.
@@ -23,4 +25,3 @@
 
 # whether we updated it or not, run the QC script
 ~/freeitathenscode/QC_Process/QC_Backend.sh
-
