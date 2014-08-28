7af646332ada654191bca7b4c8cb60dc8766fe28
frodo <frodo@243fd332-3cb7-4356-b4cc-f2eb9687c9df>
2014-08-28 20:48:15 +0000

    Prepare for Clonzilla better
    git-svn-id: svn+ssh://192.168.1.9/var/svn/Frita/freeitathenscode@167 243fd332-3cb7-4356-b4cc-f2eb9687c9df

--- image_scripts/Prepare_for_clonze.sh
+++ image_scripts/Prepare_for_clonze.sh
@@ -1,11 +1,6 @@
 #!/bin/bash +x
 declare -rx codebase="${HOME}/freeitathenscode"
 source ${codebase}/image_scripts/Common_functions || exit 12
-if [ 0 -lt $(id |grep -o -P '^uid=\d+' |cut -f2 -d=) ]
-then
-    Pauze 'ERROR,Please rerun with sudo or as root'
-    exit 4
-fi
 
 declare -r HOLDIFS=$IFS
 declare -x Runner_shell_hold=$-
@@ -54,27 +49,26 @@ egrep -v '^\s*(#|$)' /etc/fstab |grep swap |grep UUID &&\
 Pauze "apt update ( COND: $aptcache_needs_update )"
 if [ $aptcache_needs_update == 'Y' ]
 then
-    apt-get update &>>${Messages_O} &
-    Time_to_kill $! "Running apt-get update. Details in $Messages_O"
+    sudo apt-get update &>>${Messages_O} &
+    sudo Time_to_kill $! "Running apt-get update. Details in $Messages_O"
     export aptcache_needs_update='N'
 fi
-apt-get dist-upgrade
-apt-get autoremove
-apt-get clean
+sudo apt-get dist-upgrade
+sudo apt-get autoremove
+sudo apt-get clean
+
+Pauze 'Clearing out ssh secrets (and sort-of sec*)'
+find /home/oem/.ssh -not -type d -ls -delete
 
 Pauze 'Cleaning up root files that oem used...'
-find /root/.pulse /root/.dbus/session-bus -ls -delete
-find /root/.pulse /root/.dbus/session-bus -ls
-find /root/ -name ".pulse*" -ls -delete
-find /root/ -name ".pulse*" -ls
-find ${HOME}/.ssh -not -type d -ls -delete
-find ${HOME}/.ssh -not -type d -ls
-
-Pauze 'Clearing cups settings (if any)'
-for CUPSDEF in /etc/cups/{classes,printers,subscriptions}.conf; do if [ -f ${CUPSDEF}.O ];then sudo cp -v ${CUPSDEF}.O $CUPSDEF;bn=$(basename $CUPSDEF);sudo find /etc/cups/ -name "${bn}*" -exec sudo md5sum {} \; -exec sudo ls -l {} \; ;else :;fi;done
+sudo find /root/.pulse /root/.dbus/session-bus -ls -delete
+sudo find /root/ -name ".pulse*" -ls -delete
 
 Pauze 'Removing QC Test Logs'
-rm -v ${HOME}{,/Desktop}/QC*log
+find ${HOME} -type f -name 'QC*log' -ok rm -v {} \;
+
+Pauze 'Clearing cups local printer settings (if any)'
+sudo find /etc/cups -type f -name 'printers.conf*' -ok sudo rm -v {} \;
 
 Pauze 'Purge udev rules'
 rm -v /etc/udev/rules.d/*
@@ -85,7 +79,10 @@ free
 lsb_release -a
 
 Pauze 'Remove Cache files'
-for CD in $(find ${HOME}/ -depth -type d -not -empty -iname '*cache*'); do rm -rv ${CD}/*; done
+select Cachedir in $(find ${HOME}/.cache -depth -mindepth 1 -maxdepth 3 -type d -not -empty)
+do
+    find $Cachedir -type f -ok rm -v {} \;
+done
 
 Pauze 'Manually remove remaining oem-owned with rm -riv /var/lib/sudo/oem/*'
 
