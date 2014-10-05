3acf803a2cb12aed97075584c51706406afa7ee6
frodo <frodo@243fd332-3cb7-4356-b4cc-f2eb9687c9df>
2014-10-05 21:53:26 +0000

    is that the real (new) prepare_for_imaging huh
    git-svn-id: svn+ssh://192.168.1.9/var/svn/Frita/freeitathenscode@186 243fd332-3cb7-4356-b4cc-f2eb9687c9df

--- image_scripts/Prepare_for_imaging.sh
+++ image_scripts/Prepare_for_imaging.sh
@@ -1,61 +1,55 @@
 #!/bin/bash
-if [ 0 -ne $(id |grep -o -P '^uid=\d+' |cut -f2 -d=) ]
-then
-    Pauze 'if any problems rerun with sudo or as root'
-fi
+[[ 0 -ne $(id |grep -o -P '^uid=\d+' |cut -f2 -d=) ]] &&\
+    read -p'NOTE: Permission Problems? Rerun with sudo (i.e., as root).<ENTER>' -t5
 
-# Establish base of version-controlled code tree.
-sourcebase="${HOME}/freeitathenscode"
+This_script=$(basename $0)
+This_script_dir=$(dirname $0)
 
-UserSet_sourcebase() {
-    declare -a prompted_sourcebase_a
-    echo 'Normally FreeIT Code is in '$sourcebase
-    read -p 'Your Code Location? ' -a prompted_sourcebase_a
-    hold_sourcebase=${prompted_sourcebase_a[@]}
-    if [[ "$hold_sourcebase" =~ 'EXIT' ]]
+distro_name=''
+Set_Confirm_distro_name() {
+    distro_name=$1
+    [[ -z $distro_name ]] && return 40
+
+    echo -e "\n\e[1;32;40mEvaluating your input: ${distro_name}\n"
+    read -p'<ENTER>' -t3
+
+    Set_sys_rpts_distro_name;RCxS=$?
+    if [ $RCxS -gt 0 ]
     then
-        exit 2
+        echo 'Cant confirm your distro name '\
+            $distro_name' against the system name(s)'
+        read -p'<ENTER>' -t3
+        return 2
     fi
 
-    hold_sourcebase=${prompted_sourcebase_a[0]}
-    [[ -d $hold_sourcebase ]] || exit 13
-    sourcebase=hold_sourcebase
+    if [ "${distro_name}." == "${sys_rpts_distro_name}." ]
+    then
+        echo 'System distro value ('${sys_rpts_distro_name}\
+    ') agrees with your input ('${distro_name}').'
+        read -p'<ENTER>' -t3
+        return 0
+    fi
+
+    echo 'System distro value ('\
+        ${sys_rpts_distro_name}\
+          '): mismatch (however slight) with input: ('\
+        ${distro_name}').'
+    read -p'<ENTER>' -t3
+
+    return 3
 }
 
-[[ -d $sourcebase ]] || UserSet_sourcebase
-
-# Make the version-controlled tree - sourcebase --
-#  -- an unchangable value (-r)
-#  -- let child processes inherit (-x)
-declare -rx sourcebase
-
-# Establish location of Common Functions within sourcebase
-codebase=${sourcebase}'/image_scripts'
-[[ -d $codebase ]] || exit 14
-declare -rx codebase
-source ${codebase}/Common_functions || exit 15
-
-declare -r HOLDIFS=$IFS
-declare -x Runner_shell_hold=$-
-declare -rx Messages_O=$(mktemp -t "Prep_log.XXXXX")
-declare -rx Errors_O=$(mktemp -t "Prep_err.XXXXX")
-declare -x aptcache_needs_update='Y'
-declare -x refresh_git='Y'
-refresh_updatedb='N'
-refresh_svn='N'
-
-FreeIT_image='FreeIT.png'
-distro_name=''
-
-sys_rpts_distro_name_Set() {
+sys_rpts_distro_name=0
+Set_sys_rpts_distro_name() {
 
     sys_rpts_distro_name=$(lsb_release -a 2>/dev/null\
             |grep '^Distributor ID:'|cut -f2 -d:\
-            |sed -e 's/^[ \t]*//'|tr -d '\n')
+            |sed -e 's/^[ \t]*//')
 
     if [ -n ${sys_rpts_distro_name} ]
     then
-        Pauze "System reports distro as ${sys_rpts_distro_name}."
+        echo 'System reports distro as '${sys_rpts_distro_name}'.'
+        read -p'<ENTER>' -t3
         return 0
     fi
 
@@ -63,7 +57,8 @@ sys_rpts_distro_name_Set() {
     if [ "${SESSION}." == 'Lubuntu.' ]
     then
         sys_rpts_distro_name=$SESSION
-        Pauze 'Using session name '$distro_name' as distribution.'
+        echo 'Using session name '$sys_rpts_distro_name' as distribution.'
+        read -p'<ENTER>' -t3
         return 0
     fi
 
@@ -71,45 +66,43 @@ sys_rpts_distro_name_Set() {
     return 12
 }
 
-Confirm_distro_name_Set() {
-    distro_name=$1
-    [[ -z $distro_name ]] && return 40
+UserSet_sourcebase() {
 
-    prettyprint 'n,1,32,40,M,n' "Evaluating your input: ${distro_name}"
+    declare -a prompted_sourcebase_a
+    echo 'Normally FreeIT Code is in '$sourcebase
+    read -p 'Your Code Location? ' -a prompted_sourcebase_a
+    hold_sourcebase=${prompted_sourcebase_a[@]}
+    [[ "$hold_sourcebase" =~ 'EXIT' ]] && exit 2
 
-    sys_rpts_distro_name_Set;RCxS=$?
-    if [ $RCxS -gt 0 ]
-    then
-        Pauze 'Cant confirm your distro name '\
-        $distro_name' against the system name(s)'
-        return 2
-    fi
+    hold_sourcebase=${prompted_sourcebase_a[0]}
+    [[ -d $hold_sourcebase ]] || exit 13
+    sourcebase=hold_sourcebase
 
-    if [ "${distro_name}." == "${sys_rpts_distro_name}." ]
-    then
-        Pauze "System distro value (${sys_rpts_distro_name}) agrees with your input (${distro_name})."
-        return 0
-    fi
-
-    Pauze 'System distro value ('\
-        ${sys_rpts_distro_name}\
-          '): mismatch (however slight) with input: ('\
-        ${distro_name}').'
-    return 3
+    return 0
 }
 
-while getopts 'jRuVGhd:i:' OPT
+declare -x Runner_shell_hold=$-
+declare -x aptcache_needs_update='Y'
+refresh_updatedb='N'
+refresh_svn='N'
+declare -x refresh_git='Y'
+
+# Establish base of version-controlled code tree.
+sourcebase="${HOME}/freeitathenscode"
+
+Optvalid='jVRuGhd:b:'
+while getopts $Optvalid OPT
 do
     case $OPT in
         j)
+            echo \$- 'captured as '$Runner_shell_hold
             Runner_shell_hold=${Runner_shell_hold}'i'
+            echo '...now set to '$Runner_shell_hold
+        read -p'<ENTER>' -t3
             ;;
         d)
-            Confirm_distro_name_Set $OPTARG;RCx1=$?
+            Set_Confirm_distro_name $OPTARG;RCx1=$?
             [[ $RCx1 -gt 11 ]] && exit $RCx1
-            ;;
-        i)
-            FreeIT_image=$OPTARG
             ;;
         R)
             export aptcache_needs_update='N'
@@ -123,33 +116,52 @@ do
         G)
             refresh_git='N'
             ;;
+        b)
+            UserSet_sourcebase
+            ;;
         h)
-            echo $(basename $0)': Valid parms are -d Distro [-R|-u|-G|-i imagefile|-h|-j]'
+            echo $This_script\
+        ': Valid options are [ -j -V -R -u -G -d{Distro} -b{SrcBase} -h]'
+            echo '(Matches up with '$Optvalid'?'
+        read -p'<ENTER>' -t3
             exit 0
-        ;;
+            ;;
         *)
-            Pauze "Unknown option: ${OPT}. Try: -d distro [ -R -u -G -i imagefile|-h|-j]"
+            echo 'Unknown option: '${OPT}'. Exiting.'
+        read -p'<ENTER>' -t3
             exit 8
             ;;
     esac
 done
 
-declare -rx Runner_shell_as=${Runner_shell_hold}
-[[ "${refresh_updatedb}." == 'Y.' ]] && updatedb &
+[[ -d $sourcebase ]] || exit 25
 
-Contact_server() {
-    if [[ $(ssh -p8222 frita@192.168.1.9 'echo $HOSTNAME') =~ 'nuvo-servo' ]]
-    then
-        Pauze 'Checked Server is valid: 192.168.1.9'
-        return 0
-    fi
-    return 1
-}
+# Make the version-controlled tree - sourcebase --
+#  -- an unchangable value (-r)
+#  -- let child processes inherit (-x)
+declare -rx sourcebase
+
+# Establish location of Common Functions within sourcebase
+codebase=${sourcebase}'/image_scripts'
+[[ -d $codebase ]] || exit 14
+declare -rx codebase
+source ${codebase}/Common_functions || exit 135
+source ${codebase}/Prepare_functions || exit 136
+
+declare -r HOLDIFS=$IFS
+declare -rx Messages_O=$(mktemp -t "Prep_log.XXXXX")
+declare -rx Errors_O=$(mktemp -t "Prep_err.XXXXX")
+
+FreeIT_image='FreeIT.png'
+
+declare -rx Runner_shell_as=${Runner_shell_hold}
+
+[[ "${refresh_updatedb}." == 'Y.' ]] && updatedb &
 
 Correct_subversion_ssh() {
     for subv_home in ${HOME}/.subversion /etc/subversion
     do
-        [[ -d ${subv_home} ]] || return 8
+        [[ -d ${subv_home} ]] || break
 
         subv_conf="${subv_home}/config"
         if [ -f ${subv_conf} ]
@@ -167,8 +179,8 @@ Correct_subversion_ssh() {
             esac
         fi
     done
-    return 0
 
+    return 0
 }
 
 Set_background() {
@@ -179,7 +191,6 @@ Set_background() {
     [[ -d "$Image_dir" ]] || return 5
     shift 2
 
-    Pauze "Checking background file location: $Image_dir / $Image_file"
     Have_BG=$(ls -l ${Image_dir}/$Image_file)
     if [ $? -gt 0 ]
     then
@@ -202,30 +213,25 @@ Get_Address_Len
 distro_name_Set() {
 
     Pauze 'No distro supplied in parms. (e.g., -d FRITAROX)...'
-    sys_rpts_distro_name_Set;RCxS=$?
+    Set_sys_rpts_distro_name;RCxS=$?
     [[ $RCxS -eq 0 ]] || return 30
 
     Answer='N'
     Pause_n_Answer 'Y|N' '...system value '${distro_name}' is used, OK? '
     [[ "${Answer}." == 'Y.' ]] || return 12
-
     distro_name=$sys_rpts_distro_name
 
     return 0
 }
+RCxD=0
+[[ -z $distro_name ]] && distro_name_Set || RCxD=$?
+No_distro_name_bye() {
+    prettyprint '1,31,40,M,n' 'Cannot Set Distro name'
+    Pauze 'Exiting....'
+    exit $RCxD
+}
+[[ $RCxD -eq 0 ]] || No_distro_name_bye
 
-if [[ -z $distro_name ]]
-then 
-    distro_name_Set;RCxD=$?
-    if [ $RCxD -ne 0 ]
-    then
-        prettyprint '1,31,40,M,n' 'Cannot Set Distro name'
-        Pauze 'Exiting....'
-        exit $RCxD
-    fi
-fi
-
-# *--* Confirm Distro name with user *--*
 Confirm_DISTRO_CPU() {
 
     distro_valid_flag='?'
@@ -261,16 +267,16 @@ Confirm_DISTRO_CPU() {
     return 0
 
 }
-
+RCxDC=0
 Confirm_DISTRO_CPU;RCxDC=$?
-if [ $RCxDC -gt 0 ]
-then
+User_no_distro_bye() {
     prettyprint '5,31,47,M,n,0' 'Exiting'
     Pauze "See you back soon!"
     exit $RCxDC
-fi
+}
+[[ $RCxDC -eq 0 ]] || User_no_distro_bye
 
-Pauze 'Check (absence of) local UUID reference for swap in fstab.'
+echo 'Check (absence of) local UUID reference for swap in fstab.'
 RCxU=1
 grep -P 'UUID.+swap' /etc/fstab && RCxU=$?
 if [ $RCxU -eq 0 ]
@@ -278,62 +284,180 @@ then
     Pauze 'fstab cAnNoT gO oN iMaGe wItH lOcAl UUID reference. Entering editor...'
     sudo vi /etc/fstab
 fi
+Pauze '(DONE) Check (absence of) local UUID reference for swap in fstab.'
 
-Pauze 'Checking swap'
+echo 'Checking swap'
 Run_Cap_Out swapoff --all --verbose
 Run_Cap_Out swapon --all --verbose
+Pauze '(DONE) Checking swap'
 
 #Pauze 'Confirm no medibuntu in apt sources'
 #egrep -v '^\s*(#|$)' /etc/apt/sources.list |grep medi && sudo vi /etc/apt/sources.list
 
-Pauze "apt update ( COND: $aptcache_needs_update )"
-if [ $aptcache_needs_update == 'Y' ]
-then
-    apt-get update &>>${Messages_O} &
-    Time_to_kill $! "Running apt-get update. Details in $Messages_O"
-    export aptcache_needs_update='N'
-fi
+echo "Checking if apt update is needed( COND: $aptcache_needs_update )"
+Run_apt_update
+Pauze "(DONE) apt update ( COND: $aptcache_needs_update )"
 
-Pauze "install subversion"
-apt-get install subversion || exit 6
-
-Pauze "Check that server address is correct and is contactable ( COND: $refresh_svn )"
-if [ "${refresh_svn}." == 'Y.' ]
-then
-    Contact_server
-    if [ $? -lt 1 ]
-    then
-        Pauze 'Check on subversion local repo status'
-        if [ -d ${sourcebase}/.svn ]
-        then
-            cd ${sourcebase}/
-            svn update
-        else
-            cd
-            Correct_subversion_ssh
-            svn co svn+ssh://frita@192.168.1.9/var/svn/Frita/freeitathenscode/
-        fi
+echo "Check that server address is correct and is contactable ( COND: $refresh_svn )"
+Contact_server() {
+    Sub_lcl_stat() {
+    echo 'Check on subversion local repo status'
+    if [ -d ${sourcebase}/.svn ]
+                then
+        cd ${sourcebase}/
+        svn update
+    else
+        apt-get install subversion
         cd
+        Correct_subversion_ssh
+        svn co svn+ssh://frita@192.168.1.9/var/svn/Frita/freeitathenscode/
     fi
-fi
+    Pauze '(DONE) Check on subversion local repo status'
+}
+    [[ $(ssh -p8222 frita@192.168.1.9 'echo $HOSTNAME') =~ 'nuvo-servo' ]]\
+        && Pauze 'Checked Server is valid: 192.168.1.9' && Sub_lcl_stat && return $?
 
-PKGS='lm-sensors hddtemp ethtool gimp firefox dialog xscreensaver-gl libreoffice aptitude vim flashplugin-installer htop inxi vrms mintdrivers gparted terminator hardinfo'
-Pauze 'Install necessary packages: ' $PKGS
-apt-get install $PKGS
+    return 1
+}
+[[ "${refresh_svn}." == 'Y.' ]] && Contact_server
+Pauze "(DONE) Check that server address is correct and is contactable ( COND: $refresh_svn )"
 
-if [ $address_len -eq 32 ]
-then
-    Answer='N'
-    Pause_n_Answer 'Y|N' 'WARN,Install oem-config packages (Default '$Answer')?'
-    if [ "${Answer}." == 'Y.' ]
+Establish_ppa_repo_sourcefile() {
+    local pkg_name=$1
+    shift;ppa_name=$@
+    search_ppa_name=$(echo $ppa_name|tr '/' '-')
+    Pauze 'Beginning search for source.d ppa '$search_ppa_name'...'
+    RCxRo=1
+    find /etc/apt/sources.list.d/ -type f|grep $search_ppa_name && RCxRo=$?
+    if [ $RCxRo -eq 0 ]
     then
-    PK_OEM='oem-config oem-config-gtk oem-config-debconf ubiquity-frontend-[dgk].*'
-    apt-get install $PK_OEM
+        read -p'Found ppa, returning. <ENTER(t=8)>' -t8
+        return 0
     fi
-fi
+    apt_repo_name='ppa:'$ppa_name
+    echo -n $apt_repo_name
+    read -p' <Add to APT Repos?>' -a ANS || return 4
+    [[ ${#ANS[*]} -eq 0 ]] && return 0
+    if [ ${ANS[0]} == 'Y' ]
+    then
+        add-apt-repository $apt_repo_name || return $?
+	return 0
+    fi
+
+    return $?
+}
+
+RCxPK=-1
+echo 'Install necessary packages'
+Install_packages_from_file_list() {
+    local pkg_file=$1
+    RCz=0
+
+    Process_package() {
+        IFS=','
+        declare -ra pkg_info_a=($1)
+        IFS=$HOLDIFS
+
+        declare -r pkg_info_L=${#pkg_info_a[*]}
+        pkg_name=${pkg_info_a[0]}
+        pkg_by_addr=${pkg_info_a[1]}
+        [[ pkg_by_addr -eq 0 ]] && pkg_by_addr=$address_len
+        if [ $pkg_by_addr != $address_len ]
+        then
+            echo 'Skipping package '$pkg_name' on '$address_len' box.'
+            return 4
+        fi
+
+	Check_extra() {
+            local pkg_name=$1
+            shift 1
+	    pkg_extra=$@
+	    [[ -z $pkg_extra ]] && return 4
+            IFS='\='
+            declare -a extra_a=($pkg_extra)
+            IFS=$HOLDIFS
+            declare extra_L=${#extra_a[*]}
+            [[ $extra_L -gt 1 ]] || return 5
+            case ${extra_a[0]} in
+                ppa)
+                    Establish_ppa_repo_sourcefile $pkg_name ${extra_a[1]}
+                    RCxPPA=$?
+                    if [ $RCxPPA -gt 10 ]
+                    then
+                    return $RCxPPA
+                    fi
+                    if [ $RCxPPA -gt 0 ]
+                    then
+                    return 0
+                    fi 
+                    ;;
+                *)
+                    echo 'Unknown Extra Code:'$pkg_extra' for '$pkg_name
+                    return 6
+                    ;;
+                esac
+                return 115
+       }
+       RCxE=0
+       [[ $pkg_info_L -gt 3 ]] && Check_extra $pkg_name ${pkg_info_a[3]} || RCxE=$?
+       [[ $RCxE -gt 10 ]] && return $RCxE
+
+       Pkg_by_distro_session() {
+           distro_session=$1
+
+           Pkg_purge() {
+               echo -n $pkg_name' '
+               read -p'<Purge?>' -a ANS || return 0
+    	       [[ ${#ANS[*]} -eq 0 ]] && return 0
+               if [ ${ANS[0]} == 'Y' ]
+               then
+                   Apt_purge $pkg_name || return $?
+               fi
+           }
+           Pkg_add() {
+               echo -n $pkg_name' '
+	       read -p'<Install/Upgrad3(Y|n)?>' -a ANS || return 0
+	       [[ ${#ANS[*]} -eq 0 ]] && return 0
+               if [ ${ANS[0]} == 'Y' ]
+               then
+                   Apt_install $pkg_name || return $?
+               fi
+           }
+           case $distro_session in
+              NONE)
+                  Pkg_purge ||return 20
+                  ;;
+              ALL)
+                  Pkg_add || return 21
+                  ;;
+              $distro_generia)
+                  Pkg_add || return 21
+                  ;;
+           esac
+           return 1
+        }
+        Pkg_by_distro_session ${pkg_info_a[2]};RCxDS=$?
+    }
+    for pkg_info_csv in $(grep -v '^#' $pkg_file)
+    do
+        Process_package $pkg_info_csv;RCa=$?
+        if [ $RCa -gt 0 ]
+        then
+            echo 'Problem with package '$pkg_name
+        ((RCz+=$RCa))
+        fi 
+    done
+    return $RCz
+}
+for pkg_file in $(find $This_script_dir -maxdepth 1 -type f -name 'Packages*')
+do
+    RCxPK=0
+    Install_packages_from_file_list $pkg_file || RCxPK=$?
+done
+[[ $RCxPK -ne 0 ]] && echo 'Problems Installing Packages:'$RCxPK
+Pauze '(DONE) Install necessary packages'
 
 set -u
-
 Pauze 'Try to set Frita Backgrounds'
 backmess='Background Set?'
 case $distro_generia in
@@ -344,7 +468,9 @@ case $distro_generia in
         Backgrounds_location='/usr/share/backgrounds'
         ;;
 esac
+Pauze 'DONE: set Frita Backgrounds'
 
+echo 'Checking background file location: $Image_dir / $Image_file'
 Set_background $FreeIT_image $Backgrounds_location
 bg_RC=$?
 case $bg_RC in
@@ -357,8 +483,8 @@ case $bg_RC in
     *) backmess="Serious problems with setting background. RC=${bg_RC}"
     ;;
 esac
-
-Pauze "Response from setting Frita Backgrounds was $backmess"
+echo "Response from setting Frita Backgrounds was $backmess"
+Pauze "(DONE) Checking background file location: $Image_dir / $Image_file"
 
 case $distro_generia in
     mint)
@@ -369,50 +495,17 @@ case $distro_generia in
         ;;
 esac
 
-Pauze 'PPAs for firefox and gimp'
-if [ 0 -eq $(find /etc/apt/sources.list.d/ -type f -name 'mozillateam*' |wc -l) ];then
-    echo -n 'PPA: for firefox?'
-    read Xr
-    case $Xr in
-    y|Y)
-    add-apt-repository ppa:mozillateam/firefox-next
-    ;;
-    *)
-    echo 'ok moving on...'
-    ;;
-    esac
-fi
-if [ 0 -eq $(find /etc/apt/sources.list.d/ -type f -name 'otto-kesselgulasch*' |wc -l) ];then
-    echo -n 'PPA: for gimp?'
-    read Xr
-    case $Xr in
-    y|Y)
-    add-apt-repository ppa:otto-kesselgulasch/gimp
-    ;;
-    *)
-    echo 'ok moving on...'
-    ;;
-    esac
-fi
-
-Pauze 'mint and mate specials'
-if [ $address_len -eq 32 ]
-then
-    if [ $distro_generia == 'mint' ]
-    # This is actually specific to xfce: mint (32?).
-    then
-        apt-get install gnome-system-tools 
-        dpkg -l gnome-system-tools
-        Pauze 'Have gnome-system-tools?'
-    fi
+echo 'mint and mate specials'
+if [ $address_len -eq 64 ]
 else
     grep -o -P '^OnlyShowIn=.*MATE' /usr/share/applications/screensavers/*.desktop 
     Pauze 'Mate Desktop able to access xscreensavers for ant spotlight?'
 fi
+Pauze '(DONE) mint and mate specials'
 
 case $distro_generia in
     lubuntu|ubuntu|mint)
-        Pauze 'Run BPR Code'
+        echo 'Run BPR Code'
         [[ -f ${codebase}/BPR_custom_prep.sh ]] &&\
             ${codebase}/BPR_custom_prep.sh
         Pauze "Run BPR: Last return code: $?"
@@ -458,7 +551,5 @@ then
     (while [ ! -e $Mark_nouser_nogroup_fix_run ];do sleep 30;ps -ef |awk '{print $2}' |egrep "$PIDnu|$PIDng" >/dev/null||touch $Mark_nouser_nogroup_fix_run;done;chmod -c 600 $Mark_nouser_nogroup_fix_run || logger -t 'Prepare2Image' 'Problem concluding Nouser Nogroup fix') &
 fi
 
-#Answer='Y'
-#Pause_n_Answer 'Y|N' 'INFO,Check (absence of) local UUID reference for swap in fstab.(Default '$Answer')?'
-#if [ "${Answer}." == 'Y.' ]
-    #egrep -v '^\s*(#|$)' /etc/fstab\
+#PKGS='lm-sensors hddtemp ethtool gimp firefox dialog xscreensaver-gl libreoffice aptitude vim flashplugin-installer htop inxi vrms mintdrivers gparted terminator hardinfo'
+
