e37d6faac13118d4d29d6125cf3bc2eef6cce7ab
frodo <frodo@243fd332-3cb7-4356-b4cc-f2eb9687c9df>
2015-05-07 20:40:47 +0000

    image_scripts remove or move dot.config lubuntu add Select_for_Master_Imaging to replace Prepare_for...
    git-svn-id: svn+ssh://192.168.1.9/var/svn/Frita/freeitathenscode@267 243fd332-3cb7-4356-b4cc-f2eb9687c9df

---
+++ image_scripts/sysprefs.js
@@ -0,0 +1,14 @@
+user_pref("browser.safebrowsing.enabled", true);
+user_pref("browser.safebrowsing.malware.enabled", true);
+user_pref("general.autoScroll", false);
+user_pref("general.smoothScroll", false);
+#ser_pref("browser.startup.homepage", "https://google.com");
+user_pref("browser.search.defaultenginename", "Google);
+#ser_pref("browser.search.defaultthis.engineName", "Google");
+user_pref("browser.search.order.1", "Google");
+#ser_pref("browser.search.defaulturl", "https://www.google.com/search?ie=UTF-8&oe=UTF-8&q=");
+user_pref("browser.tabs.animate", false);
+#ser_pref("ui.submenuDelay", "0");
+user_pref("security.tls.version.min", "1");
+user_pref("keyword.URL", "https://www.google.com.my/search?q=");
+user_pref("browser.newtabpage.enhanced", true)
